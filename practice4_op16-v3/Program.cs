5f6554f35ee4bac12008fbff3ec3d0665d898fdc
LunArt3481 <b9172148022b@gmail.com>
2026-04-21 05:14:29 +0400

    Add files via upload

---
+++ practice4_op16-v3/Program.cs
@@ -0,0 +1,45 @@
+﻿static void Triangle(char symbol, int rows)
+{
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            Console.Write($"{symbol} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//char s = '$';
+
+Console.Write("Введите символ: ");
+char symbol = Console.ReadKey().KeyChar;
+
+Console.WriteLine("\n");
+
+int rows;
+
+Console.Write("Введите количество строк (1-20): ");
+
+while (true)
+{
+    if (!int.TryParse(Console.ReadLine(), out rows))
+    {
+        Console.Write("Ошибка ввода. Попробуйте снова: ");
+        continue;
+    }
+    if (rows <= 0 || rows > 20)
+    {
+        Console.Write("Пожалуйста, введите число от 1 до 20: ");
+        continue;
+    }
+    break;
+}
+
+
+Triangle(symbol, rows);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\nНажмите для завершения. . .");
+Console.ResetColor();
+Console.ReadKey();
